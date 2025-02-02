e45c0bc21b00bd35577d109b3e40ef0ed85f9b4f
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-02-02 16:16:30 +0200

    adapt the design section

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ $(function () {
         slidesToShow: 4,
         variableWidth: true,
         prevArrow: '<img class="arrow arrow-prev" src="images/icon-arrow-prev.svg" alt="" />',
-        nextArrow: '<img class="arrow arrow-next" src="images/icon-arrow-next.svg" alt="" />'
+        nextArrow: '<img class="arrow arrow-next" src="images/icon-arrow-next.svg" alt="" />',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    variableWidth: false,
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 })
