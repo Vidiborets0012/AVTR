2bf5f9b89b0021944cec6357891ccff8aeec2b44
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-02-02 11:34:52 +0200

    add common styles for the design-slider block

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img class="arrow arrow-prev" src="images/icon-arrow-prev.svg" alt="" />',
+        nextArrow: '<img class="arrow arrow-next" src="images/icon-arrow-next.svg" alt="" />'
+    });
+})
