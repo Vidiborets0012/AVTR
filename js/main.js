328890a6e0106325a3771f5df8ae03569aacf434
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-02-02 17:54:44 +0200

    add corrections to the slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
     $('.design-slider').slick({
         dots: false,
-        slidesToShow: 4,
-        variableWidth: true,
+        slidesToShow: 2,
+        variableWidth: false,
         prevArrow: '<img class="arrow arrow-prev" src="images/icon-arrow-prev.svg" alt="" />',
         nextArrow: '<img class="arrow arrow-next" src="images/icon-arrow-next.svg" alt="" />',
         responsive: [
